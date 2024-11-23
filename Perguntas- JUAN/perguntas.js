c24a089a6d091eb637cc46d26d9987ae5191ec96
3CJUANfigueiredo <00001082692013sp@al.educacao.sp.gov.br>
2024-11-23 17:00:17 -0300

    Add files via upload

---
+++ Perguntas- JUAN/perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Fisica',
+    'O que é Física?',
+    'Física é a ciência que estuda a natureza, comportamento e propriedades da matéria e energia.'
+)
+
+criaCartao(
+    'Fisica',
+    'Quais são os principais ramos da Física?',
+    'Física Clássica, Física Moderna, Física Teórica, Física Experimental, Física Aplicada.'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual é a diferença entre massa e peso?',
+    'Massa é a quantidade de matéria em um objeto, enquanto peso é a força exercida pela gravidade sobre esse objeto.'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual é a segunda lei de Newton?',
+    'A força aplicada a um objeto é igual à massa do objeto multiplicada pela aceleração.'
+)
+
+criaCartao(
+    'Fisica',
+    ' O que é impulso?',
+    ' Impulso é a mudança de momento linear de um objeto.'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual é a diferença entre velocidade e aceleração?',
+    'Velocidade é a taxa de mudança de posição, enquanto aceleração é a taxa de mudança de velocidade.'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual é a primeira lei da Termodinâmica?',
+    'A energia não pode ser criada nem destruída, apenas convertida.'
+)
+
+criaCartao(
+    'Fisica',
+    'O que é entropia?',
+    'Entropia é a medida da desordem ou aleatoriedade de um sistema.'
+)
